4f089faf45655eaa66bb31f13b4abf7c8ef229f9
Muneue SUWA <m_suwa@s.okayama-u.ac.jp>
2021-09-20 21:14:43 +0900

    Code refactoring

--- src/get_bluetooth_info.py
+++ src/get_bluetooth_info.py
@@ -1,26 +1,33 @@
-#coding: utf-8
 import subprocess
 import csv
-from time import sleep
+
 
 def read_address():
     file_name = 'ble-address.csv'
-    f = open(file_name,'r')
+    f = open(file_name, 'r')
     blelist = list(csv.reader(f))
     return blelist
 
+
 def get_bluetooth_info(bleaddress):
-    procs=[]
+    procs = []
     for add in bleaddress:
         print(add[0])
-        proc=subprocess.Popen(['hcitool','name',add[0]], encoding='UTF-8', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        command_str = f"hcitool name {add[0]}"
+        proc = subprocess.Popen(
+            command_str.split(),
+            encoding='UTF-8',
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
         procs.append(proc)
 
-    result=[]
+    result = []
     for proc in procs:
         result.append(proc.communicate())
-    
+
     return result
+
 
 def main():
     blelist = read_address()
@@ -30,4 +37,9 @@ def main():
     print(res)
     return 0
 
-main()
+
+if __name__ == "__main__":
+    from time import sleep
+    for i in range(60):
+        main()
+        sleep(1)
