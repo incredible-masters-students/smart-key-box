ab7a0101e2eb2d657261c85e681d020bf13b366e
Muneue SUWA <m_suwa@s.okayama-u.ac.jp>
2021-09-21 16:22:42 +0900

    Add class: loggerを簡単に追加できるようにした

--- src/get_bluetooth_info.py
+++ src/get_bluetooth_info.py
@@ -1,45 +1,64 @@
 import subprocess
 import csv
+import logging
+import shlex
+from time import sleep
+
+from read_settings import PROJ_DIR
 
 
-def read_address():
-    file_name = 'ble-address.csv'
-    f = open(file_name, 'r')
-    blelist = list(csv.reader(f))
-    return blelist
+class SmartphoneBluetoothInformation:
+    def __init__(
+        self, logger: logging.Logger,
+        hcitool_timeout: int = 3
+    ) -> None:
+        self.CSV_FILENAME = PROJ_DIR / "bluetooth_addresses.csv"
+        self.logger = logger
+        self.hcitool_timeout = hcitool_timeout
 
+    def get_bluetooth_info(self) -> list:
+        with open(self.CSV_FILENAME, 'r') as csv_f:
+            reader = csv.reader(csv_f)
+            addresses = [row for row in reader]
 
-def get_bluetooth_info(bleaddress):
-    procs = []
-    for add in bleaddress:
-        print(add[0])
-        command_str = f"hcitool name {add[0]}"
-        proc = subprocess.Popen(
-            command_str.split(),
-            encoding='UTF-8',
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-        )
-        procs.append(proc)
+        processes = []
+        for name, address in addresses:
+            process = {"name": name, "result": (None, None)}
+            command_str = f"hcitool name {address}"
+            # command_str = "sleep 10"
+            process["popen"] = subprocess.Popen(
+                shlex.split(command_str),
+                encoding='UTF-8',
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                text=True,
+            )
+            processes.append(process)
 
-    result = []
-    for proc in procs:
-        result.append(proc.communicate())
+        for process in processes:
+            try:
+                process["result"] =\
+                    process["popen"].communicate(timeout=self.hcitool_timeout)
+            except subprocess.TimeoutExpired:
+                process["popen"].kill()
 
-    return result
+        persons_around_rpi = []
+        for process in processes:
+            stdout, stderr = process["result"]
+            if stderr:
+                self.logger.error(f"Error bluetooth info: {stderr}")
+            if stdout:
+                persons_around_rpi.append(process["name"])
 
-
-def main():
-    blelist = read_address()
-    print(blelist)
-
-    res = get_bluetooth_info(blelist)
-    print(res)
-    return 0
+        return persons_around_rpi
 
 
 if __name__ == "__main__":
-    from time import sleep
+    from time import sleep, time
+    logger = logging.getLogger(__name__)
+    sbi = SmartphoneBluetoothInformation(logger)
     for i in range(60):
-        main()
+        old_time = time()
+        print(sbi.get_bluetooth_info())
+        print(f"time {i}: {time()-old_time}")
         sleep(1)
